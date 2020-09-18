baa4272c7fbb7e1090bdb947d6a5225bf938ad55
manncodes <manncodes@gmail.com>
2020-09-18 19:27:59 +0530

    Q1

---
+++ Graph Algo/test.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,a,n) for (int i=a;i<n;i++)
+#define per(i,a,n) for (int i=n-1;i>=a;i--) 
+#define ll long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define all(x) x.begin(),x.end()
+#define ar array
+#define vt vector
+#define sz(x) (int)(x).size()
+
+ll n,m;
+
+void solve(){ 
+    cin>>n>>m;
+    cout<<n<<m; 
+    
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+
+     #ifndef ONLINE_JUDGE 
+         freopen("input.txt", "r", stdin);
+         //freopen("output.txt", "w", stdout);
+     #endif
+    
+        solve();
+ 
+    //cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+    return 0;
+}
